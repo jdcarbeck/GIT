69924dbc970a7a15227214a84151aed493bcf56d
jdcarbeck <jdcarbeck@gmail.com>
2018-11-15 12:46:21 +0000

    Project now will sucessfully get the commit information for a given owner and repo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # GIT-IO
+This project looks at commits for a given repository and gives a graph displaying
+the code churn and efficiency over time.
+## Setup before running
+This project requires the addition of `Auth.hs` in the src directory. This file
+should contain the following code, where the two strings are replaced with your
+username and password for github.com:
+ ```Haskell
+ module Auth where
+
+ import qualified GitHub as GH
+
+ getAuth :: GH.Auth
+ getAuth = GH.BasicAuth "USERNAME" "PASSWORD"
+ ```
--- src/Lib.hs
+++ src/Lib.hs
@@ -8,6 +8,7 @@ import qualified Data.ByteString as B
 import qualified Data.Vector as V
 import GHC.Generics
 import Data.Time
+import Auth
 
 {-
 getAuth :: B.ByteString -> GH.Auth
@@ -19,6 +20,10 @@ getToken = do
   return token
 -}
 
+--These are the paramerters that will need to change to query different results
+owner = "howtographql"
+repo = "howtographql"
+
 data CommitInfo = CommitInfo {
        timeOfCommit :: UTCTime
      , totalLines :: Int
@@ -26,64 +31,55 @@ data CommitInfo = CommitInfo {
      , delLines :: Int
      } deriving (Generic, Show)
 
-
---TODO: Need to request each commit to get the commit information
---TODO: Still need to sort out authentication
-
-
-getCommit :: IO (Either GH.Error GH.Commit)
-getCommit = do
-  response <- GH.executeRequest' $
-              GH.commitR "jdcarbeck" "GIT" "d4ed25f7715f704025a5dc8d1546cf291f29962c"
-  return response
---This function is the organisation and repo that is to be analyse
-getAllCommits :: IO (Either GH.Error (V.Vector GH.Commit))
-getAllCommits = do
-  response <- GH.executeRequest' $
-              GH.commitsForR "jdcarbeck" "GIT" GH.FetchAll
-  return response
-
 func = do
   response <- getAllCommits
   case response of
     (Left error) -> return []
-    (Right commitsList) -> return $ (V.toList commitsList)
+    (Right commitsList) -> do
+      commitInfo <- getInfoFromCommits (V.toList commitsList)
+      return commitInfo
+
+getAllCommits :: IO (Either GH.Error (V.Vector GH.Commit))
+getAllCommits = do
+  response <- GH.executeRequest getAuth $
+              GH.commitsForR owner repo GH.FetchAll
+  return response
+
+getInfoFromCommits :: [GH.Commit] -> IO [CommitInfo]
+getInfoFromCommits (x:[]) = do
+  logToConsole x
+  stats <- commitStats x
+  return $ (mkCommitInfo x stats) : []
+getInfoFromCommits (x:xs) = do
+  logToConsole x
+  stats <- commitStats x
+  list <- getInfoFromCommits xs
+  return $ (mkCommitInfo x stats) : list
+
+logToConsole :: GH.Commit -> IO ()
+logToConsole commit = do
+  putStrLn $ "Request for commit: " ++ (show $ GH.untagName (GH.commitSha commit))
 
 
-mkCommitInfo :: GH.Commit -> CommitInfo
-mkCommitInfo commit = CommitInfo { timeOfCommit = getTimeOfCommit commit
-                                , totalLines = getCommitTotal $ GH.commitStats commit
-                                , newLines = getCommitAdd $ GH.commitStats commit
-                                , delLines = getCommitSub $ GH.commitStats commit
-                                }
+mkCommitInfo :: GH.Commit -> [Int] -> CommitInfo
+mkCommitInfo commit stats = CommitInfo { timeOfCommit = getTimeOfCommit commit
+                                        , newLines = (stats!!0)
+                                        , totalLines = (stats!!1)
+                                        , delLines = (stats!!2)
+                                        }
 
-getInfoFromCommits :: [GH.Commit] -> [CommitInfo]
-getInfoFromCommits [] = []
-getInfoFromCommits (x:[]) = (mkCommitInfo x) : []
-getInfoFromCommits (x:xs) = (mkCommitInfo x) : (getInfoFromCommits xs)
+commitStats :: GH.Commit -> IO [Int]
+commitStats commit = do
+  response <- GH.executeRequest getAuth $ GH.commitR owner repo (GH.commitSha commit)
+  case response of
+    (Left error) -> return []
+    (Right commit) -> return $ getStats (GH.commitStats commit)
 
---This function will take a commit and request the the specific commit to get the
---information that pertains to the stat
--- commitStats :: GH.Commit -> [Int]
-
-
-getCommitAdd :: Maybe GH.Stats -> Int
-getCommitAdd stats =
+getStats :: Maybe GH.Stats -> [Int]
+getStats stats =
   case stats of
-    (Nothing) -> 0
-    (Just stats) -> GH.statsAdditions stats
-
-getCommitSub :: Maybe GH.Stats -> Int
-getCommitSub stats =
-  case stats of
-    (Nothing) -> 0
-    (Just stats) -> GH.statsDeletions stats
-
-getCommitTotal :: Maybe GH.Stats -> Int
-getCommitTotal stats =
-  case stats of
-    (Nothing) -> 0
-    (Just stats) -> GH.statsTotal stats
+     (Nothing) -> (0:0:0:[])
+     (Just stats) -> ((GH.statsAdditions stats):(GH.statsTotal stats):(GH.statsTotal stats):[])
 
 getTimeOfCommit :: GH.Commit -> UTCTime
 getTimeOfCommit commit =  GH.gitUserDate (GH.gitCommitAuthor $
