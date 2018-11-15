6be2098a54dbfd712ce13762d52cf23451f2e63b
jdcarbeck <jdcarbeck@gmail.com>
2018-11-15 17:34:25 +0000

    changed shell script

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ touch "$2_data.json"
 cd ../
 cd GIT-IO
 stack build
-printf "\033[1;36mGreenGetting all commits for: \033[1;32mgithub.com/$1/$2\033[0m\n"
+printf "\033[1;36mGreenGetting all commits for: \033[1;32mgithub.com/$1/$2\033[0m...\n"
 stack exec -- GIT-IO-exe -i $1 $2
-cd ../
 printf "\033[0;34mDone\033[0m\n"
