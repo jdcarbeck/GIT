2f30f36cb55402463393da53a6dbe5e322bd9870
jdcarbeck <jdcarbeck@gmail.com>
2018-11-22 15:25:25 +0000

    changed to grid heatmap format

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+const WIDTH = 960;
+const HEIGHT = 430;
+
 var margin = {top: 20, right: 20, bottom: 100, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = WIDTH - margin.left - margin.right,
+    height = HEIGHT - margin.top - margin.bottom;
+    gridSize = Math.floor(width / 24),
+    legendElementWidth = gridSize*2,
+    buckets = 9,
+    colors = [ "#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    times = [ "01", "02", "03", "04", "05", "06"
+            , "07", "08", "09", "10", "11", "12"
+            , "13", "14", "15", "16", "17", "18"
+            , "19", "20", "21", "22", "23", "24"];
 
 var parseTime = d3.utcParse("%Y-%m-%dT%H:%M:%SZ");
-
-var x = d3.scaleTime().range([0,width]);
-var y = d3.scaleLinear().range([height, 0]);
-
-var line = d3.line()
-    .x(function(d) { return x(d.timeOfCommit); })
-    .y(function(d) { return y(d.churn); });
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -18,39 +23,34 @@ var svg = d3.select("body").append("svg")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+var dayLabels = svg.selectAll(".dayLabel")
+    .data(days)
+    .enter().append("text")
+      .text(function(d){ return d; })
+      .attr("x", 0)
+      .attr("y", function(d, i){ return i * gridSize; })
+      .style("text-anchor", "end")
+      .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+      .attr("class", function (d, i) {
+        return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis");
+      });
+
+var timeLabels = svg.selectAll(".timeLabel")
+    .data(times)
+    .enter().append("text")
+      .text(function(d) { return d; })
+      .attr("x", function(d, i) { return i * gridSize; })
+      .attr("y", 0)
+      .style("text-anchor", "middle")
+      .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+      .attr("class", function(d, i) {
+        return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis");
+      });
+
+var avgChurn = 0;
+var avgCommit = 0;
+var totalCommits = 0;
+
 d3.json("/data/jdcarbeck_git_data.json").then(function(data){
-  data.forEach(function(d){
-    d.timeOfCommit = parseTime(d.timeOfCommit);
-    if(d.totalLines > 0){
-      d.churn = (Math.round((d.delLines/d.totalLines)*100));
-    }
-    else {
-      console.log(d);
-    }
-  });
-
-
-  x.domain(d3.extent(data,function(d){ return d.timeOfCommit }));
-  y.domain([0,100]);
-
-  svg.append("path")
-      .data([data])
-      .attr("class", "line")
-      .attr("d", line);
-
-  svg.append("g")
-      .attr("class", "axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x)
-              .tickFormat(d3.timeFormat("%d/%m/%Y")))
-      .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", ".15em")
-        .attr("transform", "rotate(-45)");
-
-      // Add the Y Axis
-  svg.append("g")
-      .attr("class", "axis")
-      .call(d3.axisLeft(y));
+  
 });
