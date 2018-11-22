af74541192fcca9b6ac4322a4c8aa117a4fbc33e
jdcarbeck <jdcarbeck@gmail.com>
2018-11-22 18:23:51 +0000

    lates working version

--- app.js
+++ app.js
@@ -7,16 +7,19 @@ var margin = {top: 20, right: 20, bottom: 100, left: 50},
     gridSize = Math.floor(width / 24),
     legendElementWidth = gridSize*2,
     buckets = 9,
-    colors = [ "#000000","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
-    days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    colors = [ "#18A993","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+    days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
     times = [ "01", "02", "03", "04", "05", "06"
             , "07", "08", "09", "10", "11", "12"
             , "13", "14", "15", "16", "17", "18"
             , "19", "20", "21", "22", "23", "24"];
 
+var t = d3.transition()
+    .duration(750)
 
 var title =  d3.select("body").append("h1")
-      .style("font-size", "32px");
+      .style("font-size", "2em")
+      .style("font-family", "sans-serif");
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -34,7 +37,7 @@ var dayLabels = svg.selectAll(".dayLabel")
       .style("text-anchor", "end")
       .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
       .attr("class", function (d, i) {
-        return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis");
+        return ((i >= 1 && i <= 5) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis");
       });
 
 var timeLabels = svg.selectAll(".timeLabel")
@@ -55,6 +58,8 @@ var totalCommits = 0;
 
 d3.json("/data/GitSquared_edex-ui_data.json").then(function(data){
 
+  title.text(function(d){return data.user + "/" + data.repo;});
+
   var cards = svg.selectAll(".hour")
       .data(data.commits, function(d){
         var dt = new Date(Date.parse(d.timeOfCommit));
@@ -69,19 +74,16 @@ d3.json("/data/GitSquared_edex-ui_data.json").then(function(data){
         .attr("y", function(d){
           var dt = new Date(Date.parse(d.timeOfCommit));
           var day = dt.getDay();
-          return ((day) * gridSize);
+          return (((day) * gridSize));
         })
         .attr("x", function(d){
           var dt = new Date(Date.parse(d.timeOfCommit));
           var hr = dt.getHours();
-          return ((hr) * gridSize);
+          return (((hr) * gridSize));
         })
         .attr("class", "hour")
         .attr("width", gridSize)
         .attr("height", gridSize)
-        .style("fill", colors[0])
-        .style("opacity", 0.2);
-
-
-        title.text(function(d){return data.user + "/" + data.repo;});
+        .style("opacity", 0.1)
+        .style("fill", colors[0]);
 });
