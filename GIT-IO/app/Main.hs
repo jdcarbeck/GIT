8b1bfaaab8079e2e4be7bdb72d78c96b2933c8d6
jdcarbeck <jdcarbeck@gmail.com>
2018-11-23 15:47:23 +0000

    changed requesting process to be for all commits for a user

--- GIT-IO/app/Main.hs
+++ GIT-IO/app/Main.hs
@@ -14,13 +14,13 @@ main = do
   let argCount = length args
   case (argCount == 3) of
     (True) -> do
-      response <- requestGitHubStats (args!!1) (args!!2)
+      response <- requestGitHubStats (args!!2) (args!!1 == "org")
       case response of
         (Left error) -> putStrLn $ show error
         (Right response) -> do
-          encodeFile (getFilePath (args!!1) (args!!2)) response
+          encodeFile (getFilePath (args!!2)) response
       return response
 
 
-getFilePath :: String -> String -> FilePath
-getFilePath owner repo = "../data/" ++ owner ++ "_" ++ repo ++ "_data.json"
+getFilePath :: String -> FilePath
+getFilePath owner = "../data/" ++ owner ++ "_data.json"
